d5906571711490ed198ac2bcef56e8b3e3cf5522
vinayak16369 <appu.vinayak@pumexinfotech.com>
2024-08-06 11:03:18 +0530

    Property API completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PropertyModule } from './property/property.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, PropertyModule],
 })
 export class AppModule {}
