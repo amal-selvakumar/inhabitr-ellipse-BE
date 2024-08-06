6870ddee5b38faeb9889746490b37d9ef61be785
vinayak16369 <appu.vinayak@pumexinfotech.com>
2024-08-06 15:06:53 +0530

    new change added

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,14 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(){
-        super({
-            datasources:{
-                db:{
-                    url:'mongodb+srv://cloud-pumex:3BUpPa2SricXqwEE@inhabitr.a4ihhlg.mongodb.net/inhabitr-backend?retryWrites=true&w=majority'
-                }
-            }
-        })
-    }
+  property: any;
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'mongodb+srv://cloud-pumex:3BUpPa2SricXqwEE@inhabitr.a4ihhlg.mongodb.net/inhabitr-backend?retryWrites=true&w=majority',
+        },
+      },
+    });
+  }
 }
